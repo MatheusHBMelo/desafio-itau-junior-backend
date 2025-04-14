e727f65f0088e45aaea0ba1934be7948ce49b4c4
MatheusHBMelo <matheushbmelo@gmail.com>
2025-04-14 14:39:48 -0300

    fix: correcao na logica de validacao e criacao do metodo que recupera as transacoes

--- src/main/java/com/devtheus/desafio_itau/service/TransacaoService.java
+++ src/main/java/com/devtheus/desafio_itau/service/TransacaoService.java
@@ -5,25 +5,20 @@ import com.devtheus.desafio_itau.service.exceptions.UnprocessableEntity;
 import org.springframework.stereotype.Service;
 
 import java.time.OffsetDateTime;
-import java.time.ZoneOffset;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class TransacaoService {
 
-    private List<TransacaoRequestDto> transacoes = new ArrayList<>();
+    private final List<TransacaoRequestDto> transacoes = new ArrayList<>();
 
     public void criarTransacao(TransacaoRequestDto dto) {
-        if (dto.valor() == null || dto.dataHora() == null) {
-            throw new UnprocessableEntity("Todos os campos devem ser preenchidos.");
-        }
-
         if (dto.valor() < 0){
             throw new UnprocessableEntity("O valor da transação deve ser positivo.");
         }
 
-        if (dto.dataHora().isAfter(OffsetDateTime.now(ZoneOffset.UTC))) {
+        if (dto.dataHora().isAfter(OffsetDateTime.now())) {
             throw new UnprocessableEntity("A data da transação não pode ser futura.");
         }
 
@@ -32,5 +27,15 @@ public class TransacaoService {
 
     public void limparLista() {
         transacoes.clear();
+    }
+
+    public List<TransacaoRequestDto> buscarTransacoes(Integer intervalo){
+        OffsetDateTime dataHoraIntervalo = OffsetDateTime.now().minusSeconds(intervalo);
+
+        return transacoes
+                .stream()
+                .filter(transacao -> transacao.dataHora()
+                        .isAfter(dataHoraIntervalo))
+                .toList();
     }
 }
