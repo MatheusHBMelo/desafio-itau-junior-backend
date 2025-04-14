aad1a6e7bfa33692bba89d8f9e1cfc6c8be427de
MatheusHBMelo <matheushbmelo@gmail.com>
2025-04-14 10:35:19 -0300

    feat: cria conexão entre controller e service e o endpoint para limpar a lista de transacoes

--- src/main/java/com/devtheus/desafio_itau/controllers/TransacaoController.java
+++ src/main/java/com/devtheus/desafio_itau/controllers/TransacaoController.java
@@ -5,10 +5,7 @@ import com.devtheus.desafio_itau.service.TransacaoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/transacao")
@@ -18,6 +15,13 @@ public class TransacaoController {
 
     @PostMapping
     public ResponseEntity<Void> criarTransacao(@RequestBody TransacaoRequestDto dto) {
+        this.transacaoService.criarTransacao(dto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deletarTransacoes() {
+        this.transacaoService.limparLista();
+        return ResponseEntity.ok().build();
     }
 }
