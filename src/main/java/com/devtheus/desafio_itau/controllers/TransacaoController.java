703ffaf84ee7bcf6b12721827e0145cbc957ec4f
MatheusHBMelo <matheushbmelo@gmail.com>
2025-04-14 14:34:11 -0300

    feat: adiciona mapeamentos do swagger para documentacao dos endpoints

--- src/main/java/com/devtheus/desafio_itau/controllers/TransacaoController.java
+++ src/main/java/com/devtheus/desafio_itau/controllers/TransacaoController.java
@@ -2,6 +2,10 @@ package com.devtheus.desafio_itau.controllers;
 
 import com.devtheus.desafio_itau.controllers.dto.TransacaoRequestDto;
 import com.devtheus.desafio_itau.service.TransacaoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,16 +14,30 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping(path = "/transacao")
 @RequiredArgsConstructor
+@Tag(name = "API de transação", description = "Operações de criação e exclusão de transações")
 public class TransacaoController {
     private final TransacaoService transacaoService;
 
     @PostMapping
+    @Operation(summary = "Criar transação", description = "Endpoint para criar transação")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Cria uma nova transação com sucesso"),
+            @ApiResponse(responseCode = "422", description = "Erro de validação nos campos da transação"),
+            @ApiResponse(responseCode = "400", description = "Erro de requisição"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
     public ResponseEntity<Void> criarTransacao(@RequestBody TransacaoRequestDto dto) {
         this.transacaoService.criarTransacao(dto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @DeleteMapping
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Transação deletadas com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro de requisição"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
+    @Operation(summary = "Excluir transações", description = "Endpoint para excluir toda a lista de transações")
     public ResponseEntity<Void> deletarTransacoes() {
         this.transacaoService.limparLista();
         return ResponseEntity.ok().build();
