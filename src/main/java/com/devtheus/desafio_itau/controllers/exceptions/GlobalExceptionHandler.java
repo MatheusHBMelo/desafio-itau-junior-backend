aca2652612947c4900b9256dc8923df572e2740b
MatheusHBMelo <matheushbmelo@gmail.com>
2025-04-15 10:37:35 -0300

    feat: adiciona tratamento de erro para validacao de campos do dto

--- src/main/java/com/devtheus/desafio_itau/controllers/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/devtheus/desafio_itau/controllers/exceptions/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.devtheus.desafio_itau.controllers.exceptions;
 import com.devtheus.desafio_itau.service.exceptions.UnprocessableEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -17,6 +18,17 @@ public class GlobalExceptionHandler {
     public ResponseEntity<StandardError> unprocessbleEntityException(UnprocessableEntity ex, WebRequest request) {
         StandardError error = new StandardError(ex.getMessage(),
                 HttpStatus.UNPROCESSABLE_ENTITY.value(),
+                OffsetDateTime.now(),
+                request.getDescription(false)
+                        .replace("uri=", ""));
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<StandardError> methodArgumentNotValidException(MethodArgumentNotValidException ex, WebRequest request) {
+        StandardError error = new StandardError(ex.getMessage(),
+                HttpStatus.BAD_REQUEST.value(),
                 OffsetDateTime.now(),
                 request.getDescription(false)
                         .replace("uri=", ""));
