fc574c0801f5c60dfb5509b6524dfc0bae96fbbd
MatheusHBMelo <matheushbmelo@gmail.com>
2025-04-15 10:32:40 -0300

    feat: adiciona suporte a validacoes de campos

--- src/main/java/com/devtheus/desafio_itau/controllers/dto/TransacaoRequestDto.java
+++ src/main/java/com/devtheus/desafio_itau/controllers/dto/TransacaoRequestDto.java
@@ -1,10 +1,12 @@
 package com.devtheus.desafio_itau.controllers.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.OffsetDateTime;
 
 @Schema(name = "DTO de Transação", description = "Record que representa a transação")
-public record TransacaoRequestDto(@Schema(description = "Valor da transação", example = "10") Double valor,
-                                  @Schema(description = "Data e hora da transação em UTC", example = "2025-04-14T17:19:00Z") OffsetDateTime dataHora) {
+public record TransacaoRequestDto(@Schema(description = "Valor da transação", example = "10") @NotNull(message = "O valor da transação não pode ser vazio.") Double valor,
+                                  @Schema(description = "Data e hora da transação em UTC", example = "2025-04-14T17:19:00Z") @NotNull(message = "O valor do timestamp da transação não pode estar vazio.") OffsetDateTime dataHora) {
 }
