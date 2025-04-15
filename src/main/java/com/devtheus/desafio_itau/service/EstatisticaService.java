04d43172acaae6d5a75a83bd3516fe1ee22379b7
MatheusHBMelo <matheushbmelo@gmail.com>
2025-04-15 10:33:23 -0300

    feat: adiciona funcoes de logging

--- src/main/java/com/devtheus/desafio_itau/service/EstatisticaService.java
+++ src/main/java/com/devtheus/desafio_itau/service/EstatisticaService.java
@@ -3,6 +3,7 @@ package com.devtheus.desafio_itau.service;
 import com.devtheus.desafio_itau.controllers.dto.EstatisticaResponseDto;
 import com.devtheus.desafio_itau.controllers.dto.TransacaoRequestDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.DoubleSummaryStatistics;
@@ -10,18 +11,30 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class EstatisticaService {
     private final TransacaoService transacaoService;
 
     public EstatisticaResponseDto gerarEstatisticas(Integer intervalo) {
+        long startTime = System.currentTimeMillis();
+        log.info("Iniciando geração de estatisticas para intervalo {}", intervalo);
         List<TransacaoRequestDto> transacoes = this.transacaoService.buscarTransacoes(intervalo);
+        log.debug("Número de transações encontrada {}", transacoes.size());
 
         if (transacoes.isEmpty()) {
+            log.warn("Nenhuma transação encontrada para o intervalo {}, quadro de estatisticas vazio", intervalo);
             return new EstatisticaResponseDto(0L, 0.0, 0.0, 0.0, 0.0);
         }
 
         DoubleSummaryStatistics estatisticas = transacoes.stream().mapToDouble(TransacaoRequestDto::valor).summaryStatistics();
+        log.info("Estatisticas geradas - count {}, sum {}, avg {}, min {}, max {}",
+                estatisticas.getCount(),
+                estatisticas.getSum(),
+                estatisticas.getAverage(),
+                estatisticas.getMin(),
+                estatisticas.getMax());
 
+        log.debug("Operação concluída em {}ms", System.currentTimeMillis() - startTime);
         return new EstatisticaResponseDto(estatisticas.getCount(),
                 estatisticas.getSum(),
                 estatisticas.getAverage(),
--- src/main/java/com/devtheus/desafio_itau/service/TransacaoService.java
+++ src/main/java/com/devtheus/desafio_itau/service/TransacaoService.java
@@ -2,6 +2,7 @@ package com.devtheus.desafio_itau.service;
 
 import com.devtheus.desafio_itau.controllers.dto.TransacaoRequestDto;
 import com.devtheus.desafio_itau.service.exceptions.UnprocessableEntity;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.OffsetDateTime;
@@ -9,33 +10,41 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@Slf4j
 public class TransacaoService {
 
     private final List<TransacaoRequestDto> transacoes = new ArrayList<>();
 
     public void criarTransacao(TransacaoRequestDto dto) {
+        log.info("Iniciando criação de nova transação com valor {} e timestamp de {}.", dto.valor(), dto.dataHora());
+
         if (dto.valor() < 0){
+            log.error("Valor negativo recebido: {}. O valor da transação deve ser positivo.", dto.valor());
             throw new UnprocessableEntity("O valor da transação deve ser positivo.");
         }
 
         if (dto.dataHora().isAfter(OffsetDateTime.now())) {
+            log.error("Erro de validação em um dos campos - O timestamp da transação não pode ser futuro ao atual.");
             throw new UnprocessableEntity("A data da transação não pode ser futura.");
         }
 
         transacoes.add(dto);
+        log.info("Transação cadastrada corretamente. Total de transações {}.", transacoes.size());
     }
 
     public void limparLista() {
+        log.info("Iniciando exclusão de todas as transações da lista.");
         transacoes.clear();
+        log.info("Lista de transações excluida com sucesso.");
     }
 
     public List<TransacaoRequestDto> buscarTransacoes(Integer intervalo){
+        log.info("Iniciando a busca por transações entre o intervalo de {} segundos.", intervalo);
         OffsetDateTime dataHoraIntervalo = OffsetDateTime.now().minusSeconds(intervalo);
 
-        return transacoes
-                .stream()
-                .filter(transacao -> transacao.dataHora()
-                        .isAfter(dataHoraIntervalo))
-                .toList();
+        List<TransacaoRequestDto> resultado =  transacoes.stream().filter(transacao -> transacao.dataHora().isAfter(dataHoraIntervalo)).toList();
+        log.debug("Transações encontradas no intervalo: {}", resultado.size());
+
+        return resultado;
     }
 }
