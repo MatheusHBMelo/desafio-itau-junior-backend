87f69ea5fc0d887af2cb3bd797f7bbdd6fff6fd2
MatheusHBMelo <matheushbmelo@gmail.com>
2025-04-21 09:00:55 -0300

    fix: corrige um problema com a apresentacao deste campo no swagger ui

--- src/main/java/com/devtheus/desafio_itau/controllers/dto/TransacaoRequestDto.java
+++ src/main/java/com/devtheus/desafio_itau/controllers/dto/TransacaoRequestDto.java
@@ -1,12 +1,11 @@
 package com.devtheus.desafio_itau.controllers.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.OffsetDateTime;
 
 @Schema(name = "DTO de Transação", description = "Record que representa a transação")
-public record TransacaoRequestDto(@Schema(description = "Valor da transação", example = "10") @NotNull(message = "O valor da transação não pode ser vazio.") Double valor,
-                                  @Schema(description = "Data e hora da transação em UTC", example = "2025-04-14T17:19:00Z") @NotNull(message = "O valor do timestamp da transação não pode estar vazio.") OffsetDateTime dataHora) {
+public record TransacaoRequestDto(@Schema(description = "Valor da transação") @NotNull(message = "O valor da transação não pode ser vazio.") Double valor,
+                                  @Schema(description = "Data e hora da transação em UTC") @NotNull(message = "O valor do timestamp da transação não pode estar vazio.") OffsetDateTime dataHora) {
 }
